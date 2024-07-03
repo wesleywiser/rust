79110213cedc8671a8ece22d92adcca2469556e9
Wesley Wiser <wesleywiser@microsoft.com>
2024-07-03 09:39:36 -0500

    test

--- compiler/rustc_target/src/abi/call/x86_win64.rs
+++ compiler/rustc_target/src/abi/call/x86_win64.rs
@@ -1,10 +1,10 @@
-use crate::abi::call::{ArgAbi, FnAbi, Reg};
-use crate::abi::Abi;
+use crate::abi::call::{ArgAbi, FnAbi, Reg, RegKind};
+use crate::abi::{Abi, Size};
 
 // Win64 ABI: https://docs.microsoft.com/en-us/cpp/build/parameter-passing
 
 pub fn compute_abi_info<Ty>(fn_abi: &mut FnAbi<'_, Ty>) {
-    let fixup = |a: &mut ArgAbi<'_, Ty>| {
+    let fixup = |a: &mut ArgAbi<'_, Ty>, is_ret: bool| {
         match a.layout.abi {
             Abi::Uninhabited | Abi::Aggregate { sized: false } => {}
             Abi::ScalarPair(..) | Abi::Aggregate { sized: true } => match a.layout.size.bits() {
@@ -20,7 +20,14 @@ pub fn compute_abi_info<Ty>(fn_abi: &mut FnAbi<'_, Ty>) {
             }
             Abi::Scalar(_) => {
                 if a.layout.size.bytes() > 8 {
-                    a.make_indirect();
+                    if is_ret {
+                        a.cast_to(Reg {
+                            kind: RegKind::Vector,
+                            size: Size::from_bytes(a.layout.size.bytes()),
+                        });
+                    } else {
+                        a.make_indirect();
+                    }
                 } else {
                     a.extend_integer_width_to(32);
                 }
@@ -29,12 +36,12 @@ pub fn compute_abi_info<Ty>(fn_abi: &mut FnAbi<'_, Ty>) {
     };
 
     if !fn_abi.ret.is_ignore() {
-        fixup(&mut fn_abi.ret);
+        fixup(&mut fn_abi.ret, true);
     }
     for arg in fn_abi.args.iter_mut() {
         if arg.is_ignore() {
             continue;
         }
-        fixup(arg);
+        fixup(arg, false);
     }
 }
