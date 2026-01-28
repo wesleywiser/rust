0128cde16e9644c3ec121dfa03543b906f031a93
Wesley Wiser <wwiser@gmail.com>
2026-01-27 20:52:54 -0600

    Allow forbidden target features to be hard errors

--- compiler/rustc_codegen_ssa/src/errors.rs
+++ compiler/rustc_codegen_ssa/src/errors.rs
@@ -1195,12 +1195,13 @@ pub(crate) struct UnstableCTargetFeature<'a> {
 
 #[derive(Diagnostic)]
 #[diag(codegen_ssa_forbidden_ctarget_feature)]
-#[note]
-#[note(codegen_ssa_forbidden_ctarget_feature_issue)]
 pub(crate) struct ForbiddenCTargetFeature<'a> {
     pub feature: &'a str,
     pub enabled: &'a str,
     pub reason: &'a str,
+    #[note]
+    #[note(codegen_ssa_forbidden_ctarget_feature_issue)]
+    pub future_compat_note: Option<()>,
 }
 
 pub struct TargetFeatureDisableOrEnable<'a> {
